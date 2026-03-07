de033f858f45e2082f2c5127d985c71dce89844b
Muppidi Manobhi Rami Reddy <151667410+manobhi12@users.noreply.github.com>
2026-03-07 22:47:52 +0530

    Time: 0 ms (100%), Space: 19.3 MB (61.02%) - LeetHub

---
+++ 0389-find-the-difference/0389-find-the-difference.py
@@ -0,0 +1,11 @@
+class Solution:
+    def findTheDifference(self, s: str, t: str) -> str:
+        result = 0
+        
+        for c in s:
+            result ^= ord(c)
+            
+        for c in t:
+            result ^= ord(c)
+            
+        return chr(result)
